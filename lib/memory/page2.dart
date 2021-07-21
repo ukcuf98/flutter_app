40c05ea36754a75c993acb37ef6adaf0a1ca03c3
Ukcuf98 <920415463@qq.com>
2021-07-21 17:36:01 +0800

    部分代码

--- lib/memory/page2.dart
+++ lib/memory/page2.dart
@@ -47,82 +47,21 @@ class Page2 extends StatelessWidget {
                   ),
                 ),
                 Container(
-                  padding: EdgeInsets.fromLTRB(0, 260, 260, 0),
+                  padding: EdgeInsets.fromLTRB(0, 450, 210, 0),
                   child: Center(
-                      child: Image.network(//折线
-                        "http://ddiy.36588.com.cn/image/mong/1/d149b32f-f9da-417f-9913-14b2731d9243.png",
-                        height: 100,
+                      child: Image(
+                        image: AssetImage('assets/memory/page2/3.png'),
+                        height: 30,
                       )
                   ),
                 ),
                 Container(
-                  padding: EdgeInsets.fromLTRB(300, 0, 0, 500),
+                  padding: EdgeInsets.fromLTRB(0, 0, 100, 50),
                   child: Center(
-                    child: Opacity(
-                        opacity: 0.4,
-                        child: Image.network(//右上角黄色圆形
-                          "http://ddiy.36588.com.cn/image/mong/1/MXYZ-9410C59F8D364A738123524A87E778AC.png",
-                          width: 45,
-                          height: 45,
-                        )
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(0, 0, 230, 440),
-                  child: Center(
-                    child: Opacity(
-                        opacity: 0.4,
-                        child: Image.network(//左上角淡棕色圆形
-                          "http://ddiy.36588.com.cn/image/mong/1/MXYZ-4AF735A591DB492A91EC05CCE05422B1.png",
-                          width: 50,
-                          height: 50,
-                        )
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(0, 0, 100, 440),
-                  child: Center(
-                      child: Image.network(//02正方形
-                        "http://ddiy.36588.com.cn/image/mong/1/ad7c04d7-fc84-469a-bfe9-652475eb9ec0.png",
-                        width: 40,
-                        height: 40,
-                      )
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(320, 0, 0, 352),
-                  child: Center(
-                      child: Image.network(//右侧垂直排列的字
-                          "http://ddiy.36588.com.cn/image/mong/1/8ab7f407-09d3-4dec-8524-810af5927113.png",
-                          width: 180,
-                          height: 180,
-                          scale: 0.1
-                      )
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(0, 0, 220, 352),
-                  child: Center(
-                      child: Image.network(//左侧垂直排列的字
-                          "http://ddiy.36588.com.cn/image/mong/1/77f56b07-dfbf-42df-9fe8-8997b5afe0df.png",
-                          width: 100,
-                          height: 100,
-                          scale: 0.1
-                      )
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(0, 0, 210, 275),
-                  child: Center(
-                    child: Opacity(
-                        opacity: 0.4,
-                        child: Image.network(//右上角黄色圆形
-                          "http://ddiy.36588.com.cn/image/mong/1/MXYZ-9410C59F8D364A738123524A87E778AC.png",
-                          height: 30,
-                        )
-                    ),
+                    child: Image(
+                      image: AssetImage('assets/memory/page2/4.png'),
+                      width: 220,
+                    )
                   ),
                 ),
                 Container(
