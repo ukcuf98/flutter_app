aba6e8c0daf7dfab93729c9fe48bf6b03ebbfafe
Ukcuf98 <920415463@qq.com>
2021-09-29 11:11:18 +0800

    部分代码

---
+++ lib/flame/boxgame/main.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/gestures.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flame/util.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutterapp/flame/boxgame/box-game.dart';
+//
+// void main() async{
+//   Util flameUtil = Util();
+//   await flameUtil.fullScreen();
+//   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+//   BoxGame game = BoxGame();
+//   TapGestureRecognizer tapper = TapGestureRecognizer();
+//   tapper.onTapDown = game.onTapDown;
+//   runApp(game.widget);
+//   // flameUtil.addGestureRecognizer(tapper);
+//   // game
+// }
